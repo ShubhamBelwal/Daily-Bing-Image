39f0e00130d1d99a30644ebea8677550d278102d
ShubhamBelwal <36201797+ShubhamBelwal@users.noreply.github.com>
2018-05-19 19:15:00 +0530

    Add files via upload

---
+++ bing_image.py
@@ -0,0 +1,23 @@
+import requests,json,os,ctypes
+
+bing_api = "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US"
+prefix = "https://www.bing.com"
+resolution = "1366x768"
+def get_and_set_wallpaper():
+    os.makedirs('Bing',exist_ok=True)
+    dir_path = os.path.join(os.getcwd(),"Bing")
+    response = requests.get(bing_api).json()
+    url = response['images'][0]['urlbase']
+    image_url = prefix + url + "_" + resolution + ".jpg"
+    res = requests.get(image_url)
+    res.raise_for_status()
+    imageFile = open(os.path.join('Bing', os.path.basename(image_url)), 'wb')
+    imageFile.write(res.content)
+    imageFile.close()
+    image = os.path.join(dir_path, url.replace("/", " ").split()[-1]) + "_" + resolution + ".jpg"
+    SPI_SETDESKWALLPAPER = 0x0014
+    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, image, 0)
+    return
+
+if __name__ == "__main__":
+    get_and_set_wallpaper()
